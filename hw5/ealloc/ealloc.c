8cb49701d221b675662edb087e3083c76d6f4ef5
Frics <jimmy9711@naver.com>
2020-11-18 20:57:17 +0900

    주석 처리하는중ㅋㅎㅎ

--- hw5/ealloc/ealloc.c
+++ hw5/ealloc/ealloc.c
@@ -7,12 +7,29 @@ struct mm{
 	int size;
 	int flag;
 };
+
 char *start[PNUM];
 struct mm mem[PNUM][NUM];
 
 void init_alloc(){
+
 	for(int p=0; p <PNUM; p++){ 
 		start[p] = NULL;
+
+		for(int i=0; i<NUM; i++){
+			mem[p][i].adr = NULL;
+			mem[p][i].size = MINALLOC;
+			mem[p][i].flag= 0;
+		}
+	}
+
+	return;
+}
+
+void cleanup(){
+	for(int p=0; p <PNUM; p++){ 
+		start[p] = NULL;
+
 		for(int i=0; i<NUM; i++){
 			mem[p][i].adr = NULL;
 			mem[p][i].size = MINALLOC;
@@ -21,26 +38,25 @@ void init_alloc(){
 	}
 	return;
 }
-void cleanup(){
-	for(int i=0; i<PNUM; i++){
-		if(start[i] != NULL)
-			munmap(start[i], PAGESIZE);
-	}
-	return;
-}
+
 char *alloc(int size){
 	char *adr;
-
+	int valid;
 	for(int p=0; p<PNUM; p++){
+
 		if(start[p] == NULL)
 			start[p] = (char *)mmap(0, PAGESIZE, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 
 		for(int i = 0; i < NUM; i++){
-			int valid = 0;
+			valid = 0;
+
 			if(mem[p][i].flag ==0){
+
 				for(int j=i; j<NUM; j++){
+
 					if(mem[p][j].flag == 1)
 						break;
+
 					valid += MINALLOC;
 					if(size <= valid){
 						mem[p][i].size = valid;
@@ -50,20 +66,24 @@ char *alloc(int size){
 						return adr;
 					}
 				}
-			}else if(mem[p][i].size != MINALLOC){
+			}
+			else if(mem[p][i].size != MINALLOC){
 				i += mem[p][i].size/MINALLOC-1;
 				if(i>=NUM)
 					break;
 			}
 		}
 	}
+
 	return NULL;
 }
+
 void dealloc(char *adr){
+
 	for(int p = 0; p<PNUM; p++){
 		for(int i = 0; i<NUM; i++){
 			if(mem[p][i].adr == adr){
-				mem[p][i].adr = 0;
+				mem[p][i].adr = NULL;
 				mem[p][i].size = MINALLOC;
 				mem[p][i].flag = 0;
 				break;
